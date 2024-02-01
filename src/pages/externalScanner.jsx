08ec9980aaf4116b4566707081a6a8bfee0f7d75
Ralph Fuentes <academy4@talentamp.com>
2024-02-01 16:14:11 +0800

    Will go back from scratch and remove externalScanning and create a new one

--- src/pages/externalScanner.jsx
+++ src/pages/externalScanner.jsx
@@ -1,47 +1,20 @@
-import { useNavigate } from "react-router-dom"; // Import the useNavigate hook
-import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap styles
+import React, { useEffect, useState } from "react";
+import useScanDetection from "use-scan-detection";
 import "./externalScanner.css";
-import instructionsImage from "../images/Instructions.jpg" 
 
-const ExternalScanner = () => {
-  const navigate = useNavigate(); // Initialize the useNavigate hook
-  const handleSwitchLocation = () => {
-    navigate("/");
-  };
+function ExternalScanner() {
+  const [barcodeScan, setBarcodeScan] = useState("No barcode Scanned");
 
-  const handleSwitchDevice = () => {
-    // Navigate to the new page when the button is clicked
-    navigate("/qr-scanning"); // Replace "/new-page" with the desired URL
-  };
+  useScanDetection({
+    onComplete: setBarcodeScan,
+    minLength: 3,
+  });
 
   return (
-    <div className="qrScanning">
-      <div className="Titles">
-      <h5>Welcome to SupportZebra!</h5>
-      <h5>Scan your QR here to record your logs</h5>
-      </div>
-      <div className="centerImage">
-      <img className="instructionsImage" src={instructionsImage}></img>
-      <div className="buttons" class="d-flex justify-content-center gap-1 switch-btn-container">
-        <button
-          type="button"
-          className="btn btn-success back-btn"
-          onClick={handleSwitchLocation}
-        >
-          Back
-        </button>
-        <button
-          type="button"
-          className="btn btn-success switch-btn"
-          id="switch-scanning-device"
-          onClick={handleSwitchDevice}
-        >
-          Switch
-        </button>
-      </div>
-    </div>
+    <div className="external-scanner" style={{ padding: "5rem 10rem" }}>
+      <p>Barcode: {barcodeScan}</p>
     </div>
   );
-};
+}
 
 export default ExternalScanner;
---
+++ src/pages/externalScanning.jsx
@@ -0,0 +1,50 @@
+import { useNavigate } from "react-router-dom"; // Import the useNavigate hook
+import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap styles
+import "./externalScanning.css";
+import instructionsImage from "../images/Instructions.jpg";
+
+const externalScanning = () => {
+  const navigate = useNavigate(); // Initialize the useNavigate hook
+  const handleSwitchLocation = () => {
+    navigate("/");
+  };
+
+  const handleSwitchDevice = () => {
+    // Navigate to the new page when the button is clicked
+    navigate("/qr-scanning"); // Replace "/new-page" with the desired URL
+  };
+
+  return (
+    <div className="qrScanning">
+      <div className="Titles">
+        <h5>Welcome to SupportZebra!</h5>
+        <h5>Scan your QR here to record your logs</h5>
+      </div>
+      <div className="centerImage">
+        <img className="instructionsImage" src={instructionsImage}></img>
+        <div
+          className="buttons"
+          class="d-flex justify-content-center gap-1 switch-btn-container"
+        >
+          <button
+            type="button"
+            className="btn btn-success back-btn"
+            onClick={handleSwitchLocation}
+          >
+            Back
+          </button>
+          <button
+            type="button"
+            className="btn btn-success switch-btn"
+            id="switch-scanning-device"
+            onClick={handleSwitchDevice}
+          >
+            Switch
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default externalScanning;
