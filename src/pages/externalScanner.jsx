1ed2cfb81b0921250eb43c7937bf5cd814b3d3ee
Ralph Fuentes <academy4@talentamp.com>
2024-02-05 19:44:29 +0800

    Progress with externalScanner. Will work on with the bugs and fixes

--- src/pages/externalScanner.jsx
+++ src/pages/externalScanner.jsx
@@ -6,13 +6,11 @@ import { GridLoader } from "react-spinners";
 import defaultProfile from "../images/user.png";
 
 function ExternalScanner() {
-  const [isSubmitting, setIsSubmitting] = useState(false);
   const [loading, setLoading] = useState(false);
   const [barcodeScan, setBarcodeScan] = useState("");
   const [fullName, setFullName] = useState("");
   const [qrError, setQRError] = useState("");
   const [loginType, setLoginType] = useState("");
-  const [happyWorking, setHappyWorking] = useState("");
   const [log, setLog] = useState("");
   const [isDuplicate, setIsDuplicate] = useState(false);
   const [formattedDate, setFormattedDate] = useState("");
@@ -20,12 +18,14 @@ function ExternalScanner() {
   const [pic, setPic] = useState("");
   const [link, setLink] = useState("");
 
-  const [lastScanTime, setLastScanTime] = useState(
-    () => Number(sessionStorage.getItem("lastScanTime")) || null
-  );
   const navigate = useNavigate();
 
-  const MINUTES_BEFORE_SCANNING_ALLOWED = 2;
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 4500);
+  }, [barcodeScan]);
 
   const handleBarcodeComplete = (barcodeData) => {
     setLoading(true);
@@ -40,43 +40,15 @@ function ExternalScanner() {
   });
 
   useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 4500);
-  }, [barcodeScan]);
-
-  useEffect(() => {
-    if (barcodeScan.length > 0) {
-      const storedResult = localStorage.getItem("resultID");
-
-      if (!storedResult) {
-        localStorage.setItem("resultID", barcodeScan);
-        setTimeout(() => {
-          window.location.reload();
-        }, 10000);
-      } else if (barcodeScan !== storedResult) {
-        // Retrieve selectedOption from localStorage
-        const selectedOption = localStorage.getItem("selectedOption");
-        sendScannedDataToBackend(barcodeScan, selectedOption);
-      }
-      localStorage.setItem("oldResultID", storedResult);
-      console.log(storedResult, barcodeScan);
-      setTimeout(() => {
-        window.location.reload();
-      }, 10000);
-    }
-  }, [barcodeScan]);
-
-  useEffect(() => {
     const oldScannedValue = localStorage.getItem("oldResultID");
     const newScannedValue = localStorage.getItem("resultID");
 
     setIsDuplicate(oldScannedValue === newScannedValue);
   }, [localStorage.getItem("oldResultID"), localStorage.getItem("resultID")]);
 
-  const sendScannedDataToBackend = async (result, selectedOption) => {
+  const sendScannedDataToBackend = async (result) => {
     try {
+      const selectedOption = localStorage.getItem("selectedOption");
       const response = await fetch("http://localhost:4440/logsz/saveQRLog", {
         method: "POST",
         headers: {
@@ -98,18 +70,15 @@ function ExternalScanner() {
         setLoginType(data.result.type);
         setLog(data.result.log);
         setPic(data.result.pic);
-        console.log("https://app.supportzebra.net/" + data.result.pic);
 
         const logDate = new Date(data.result.log);
 
-        // Format date
         const optionsDate = { year: "numeric", month: "long", day: "numeric" };
         const formattedDateString = logDate.toLocaleDateString(
           "en-US",
           optionsDate
         );
 
-        // Format time
         const optionsTime = {
           hour: "2-digit",
           minute: "2-digit",
@@ -124,7 +93,6 @@ function ExternalScanner() {
         setFormattedDate(formattedDateString);
         setFormattedTime(formattedTimeString);
 
-        // Ensure that setQRError is called after processing data
         setQRError(""); // Clear any previous error
       }
 
@@ -135,10 +103,9 @@ function ExternalScanner() {
       console.error("Error sending data to backend:", error);
       setQRError("Error sending data to backend");
 
-      // Clear the error after 5 seconds (you can adjust the time)
       setTimeout(() => {
         setQRError("");
-      }, 0);
+      }, 5000);
     }
 
     localStorage.setItem("resultID", result);
@@ -152,14 +119,6 @@ function ExternalScanner() {
       setLink("https://app.supportzebra.net/" + pic);
     }
   }, [pic]);
-
-  useEffect(() => {
-    if (loginType === "in") {
-      setHappyWorking("Happy Working :)");
-    } else {
-      setHappyWorking("Thank you for your service :)");
-    }
-  }, [loginType]);
 
   return (
     <div className="App">
@@ -178,8 +137,8 @@ function ExternalScanner() {
                 <p>
                   Hi! {fullName} <br /> <br /> You have successfully logged{" "}
                   {loginType}. <br /> <br />
-                  {happyWorking} <br />
-                  <br /> {formattedDate} {""} {formattedTime}
+                  <br />
+                  {formattedDate} {""} {formattedTime}
                 </p>
               )}
               {qrError && <h1>{qrError}</h1>}
