502ee6b0ff8e42ad7933648c64aa68bf16b88272
Ralph Fuentes <academy4@talentamp.com>
2024-02-08 18:13:30 +0800

    Will still work on with time interval

--- src/pages/Scanner.jsx
+++ src/pages/Scanner.jsx
@@ -6,7 +6,6 @@ import { GridLoader } from "react-spinners";
 import defaultProfile from "../images/user.png";
 
 function Scanner({ onScanResultChange, action }) {
-  const [isSubmitting, setIsSubmitting] = useState(false);
   const [loading, setLoading] = useState(false);
   const [scanResult, setScanResult] = useState("");
   const [scanner, setScanner] = useState(null);
@@ -20,9 +19,48 @@ function Scanner({ onScanResultChange, action }) {
   const [pic, setPic] = useState("");
   const [link, setLink] = useState("");
   const [lastScanTime, setLastScanTime] = useState(
-    () => Number(sessionStorage.getItem("lastScanTime")) || null
+    () => Number(localStorage.getItem("lastScanTime")) || null
   );
   const navigate = useNavigate();
+
+  function success(result) {
+    const localCurrentTime = localStorage.getItem("currentTime");
+    const localCurrentEmployee = localStorage.getItem("currentEmployee");
+    const intervalTime = 120000; // 2 minutes in milliseconds
+    console.log("Local Current Time:", parseInt(localCurrentTime));
+    console.log("Local Current Employee:", localCurrentEmployee);
+    console.log("Current Time:", Date.now());
+
+    setScanResult(result);
+    onScanResultChange(result);
+    localStorage.setItem("lastScanTime", Date.now());
+    setLastScanTime(Date.now());
+    const selectedOption = localStorage.getItem("selectedOption");
+    navigate(
+      `/qr-scanning?selectedOption=${selectedOption}&lastScanResult=${result}`
+    );
+    setTimeout(() => {
+      setScanResult("");
+    }, 0);
+
+    if (result === localCurrentEmployee && localCurrentTime) {
+      const elapsedTime = Date.now() - parseInt(localCurrentTime);
+      console.log("Elapsed Time:", elapsedTime);
+      const isWithinTwoMinutes = checkIntervalTime(
+        parseInt(localCurrentTime),
+        Date.now(),
+        intervalTime
+      );
+      console.log("Is Within Two Minutes:", isWithinTwoMinutes);
+      setIsDuplicate(isWithinTwoMinutes);
+    } else {
+      sendScannedDataToBackend(result, selectedOption);
+    }
+  }
+
+  function error(err) {
+    // Handle error
+  }
 
   useEffect(() => {
     setLoading(true);
@@ -39,9 +77,7 @@ function Scanner({ onScanResultChange, action }) {
       },
       fps: 5,
     });
-
     setScanner(scannerElement);
-
     return () => {
       scannerElement.clear();
     };
@@ -49,147 +85,84 @@ function Scanner({ onScanResultChange, action }) {
 
   useEffect(() => {
     if (!scanner) return;
-
     scanner.render(success, error);
-
-    function success(result) {
-      scanner.clear();
-      setScanResult(result);
-
-      onScanResultChange(result);
-
-      sessionStorage.setItem("lastScanTime", Date.now());
-      setLastScanTime(Date.now());
-
-      // Get selectedOption from localStorage instead of URL params
-      const selectedOption = localStorage.getItem("selectedOption");
-      navigate(
-        `/qr-scanning?selectedOption=${selectedOption}&lastScanResult=${result}`
-      );
-
-      // Ensure that setScanResult is called after navigation
-      setTimeout(() => {
-        setScanResult("");
-      }, 0);
-
-      const localCurrentTime = localStorage.getItem("currentTime");
-      const localCurrentEmployee = localStorage.getItem("currentEmployee");
-      const intervalTime = 120000; // 2 minutes in milliseconds
-
-      if (result === localCurrentEmployee) {
-        const isWithinTwoMinutes = checkIntervalTime(
-          localCurrentTime,
-          Date.now(),
-          intervalTime
-        );
-
-        if (isWithinTwoMinutes) {
-          sendScannedDataToBackend(result, selectedOption);
-        } else {
-          setIsDuplicate(true); // Set isDuplicate to true if not within interval
-          return;
-        }
-      } else {
-        sendScannedDataToBackend(result, selectedOption);
-      }
-    }
-
-    function error(err) {}
-
     return () => {
       scanner.clear();
     };
-  }, [scanner, onScanResultChange, lastScanTime, navigate]);
+  }, [scanner, onScanResultChange, navigate, isDuplicate]);
 
   const sendScannedDataToBackend = async (result, selectedOption) => {
-    if (isSubmitting) {
-      return;
-    } else {
-      try {
-        const response = await fetch("http://localhost:4440/logsz/saveQRLog", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            id_num: parseInt(result),
-            scannerLocation_ID: parseInt(selectedOption),
-          }),
-        });
+    try {
+      const response = await fetch("http://localhost:4440/logsz/saveQRLog", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id_num: parseInt(result),
+          scannerLocation_ID: parseInt(selectedOption),
+        }),
+      });
 
-        const data = await response.json();
-        console.log("Backend response:", data);
+      const data = await response.json();
+      console.log("Backend response:", data);
 
-        if (!data.result) {
-          setQRError(
-            "Invalid QR code. Please scan only a Support Zebra dedicated QR code."
-          );
-          action(false);
-        } else {
-          action(false);
-          setFullName(data.result.fullname);
-          setLoginType(data.result.type);
+      if (!data.result) {
+        setQRError(
+          "Invalid QR code. Please scan only a Support Zebra dedicated QR code."
+        );
+        action(false);
+      } else {
+        action(false);
+        setFullName(data.result.fullname);
+        setLoginType(data.result.type);
+        setPic(data.result.pic);
 
-          setPic(data.result.pic);
-          // console.log("https://app.supportzebra.net/" + data.result.pic);
-          console.log(result, "sadasdasdsa");
-          const logDate = new Date(data.result.log);
-          //for time interval (getting current time scanned from DB)
-          localStorage.setItem("currentTime", data.result.log);
-          localStorage.setItem("currentEmployee", data.result.id_num);
+        const logDate = new Date(data.result.log);
+        localStorage.setItem("currentTime", data.result.log);
+        localStorage.setItem("currentEmployee", data.result.id_num);
 
-          // Format date
-          const optionsDate = {
-            year: "numeric",
-            month: "long",
-            day: "numeric",
-          };
-          const formattedDateString = logDate.toLocaleDateString(
-            "en-US",
-            optionsDate
-          );
+        const optionsDate = {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        };
+        const formattedDateString = logDate.toLocaleDateString(
+          "en-US",
+          optionsDate
+        );
 
-          // Format time
-          const optionsTime = {
-            hour: "2-digit",
-            minute: "2-digit",
-            second: "2-digit",
-            hour12: true,
-          };
-          const formattedTimeString = logDate.toLocaleTimeString(
-            "en-US",
-            optionsTime
-          );
+        const optionsTime = {
+          hour: "2-digit",
+          minute: "2-digit",
+          second: "2-digit",
+          hour12: true,
+        };
+        const formattedTimeString = logDate.toLocaleTimeString(
+          "en-US",
+          optionsTime
+        );
 
-          setFormattedDate(formattedDateString);
-          setFormattedTime(formattedTimeString);
-
-          // Ensure that setQRError is called after processing data
-          setQRError(""); // Clear any previous error
-        }
-
-        setTimeout(() => {
-          window.location.reload();
-        }, 10000);
-      } catch (error) {
-        console.error("Error sending data to backend:", error);
-        setQRError("Error sending data to backend");
+        setFormattedDate(formattedDateString);
+        setFormattedTime(formattedTimeString);
+        setQRError(""); // Clear any previous error
       }
 
-      localStorage.setItem("resultID", result);
-      console.log(localStorage.getItem("resultID"));
+      setTimeout(() => {
+        window.location.reload();
+      }, 10000);
+    } catch (error) {
+      console.error("Error sending data to backend:", error);
+      setQRError("Error sending data to backend");
     }
 
-    setIsSubmitting(false);
+    localStorage.setItem("resultID", result);
   };
 
   function checkIntervalTime(lastScanTime, currentTime, intervalTime) {
     const elapsedTime = currentTime - lastScanTime;
     return elapsedTime <= intervalTime;
   }
-
-  // over all link to access image
-  // const link = "https://app.supportzebra.net/" + pic;
 
   useEffect(() => {
     if (!pic) {
@@ -213,17 +186,18 @@ function Scanner({ onScanResultChange, action }) {
 
       if (!storedResult) {
         localStorage.setItem("resultID", scanResult);
-
         setTimeout(() => {
           window.location.reload();
         }, 10000);
       } else if (scanResult !== storedResult) {
-        // Retrieve selectedOption from localStorage
         const selectedOption = localStorage.getItem("selectedOption");
         sendScannedDataToBackend(scanResult, selectedOption);
       }
+
       localStorage.setItem("oldResultID", storedResult);
-      console.log(storedResult, scanResult);
+      console.log(storedResult);
+      console.log(scanResult);
+
       setTimeout(() => {
         localStorage.removeItem(storedResult);
         localStorage.removeItem(scanResult);
@@ -234,33 +208,31 @@ function Scanner({ onScanResultChange, action }) {
 
   return (
     <div id="scanner-cont">
-      <>
-        {!scanResult ? (
-          <div id="reader"></div>
-        ) : loading ? (
-          <GridLoader color={"#198754"} loading={loading} size={100} />
-        ) : (
-          <div>
-            <img className="profile" src={link} alt="Profile"></img>
+      {!scanResult ? (
+        <div id="reader"></div>
+      ) : loading ? (
+        <GridLoader color={"#198754"} loading={loading} size={100} />
+      ) : (
+        <div>
+          <img className="profile" src={link} alt="Profile" />
+          <br />
+          <h2>
+            {fullName && (
+              <p>
+                Hi! {fullName} <br /> <br /> You have successfully logged{" "}
+                {loginType}. <br /> <br />
+                {happyWorking} <br />
+                <br /> {formattedDate} {""} {formattedTime}
+              </p>
+            )}
+            {qrError && <h1>{qrError}</h1>}
+            {isDuplicate && (
+              <h1>Duplicate Entry. Please scan after 2 minutes</h1>
+            )}
             <br />
-            <h2>
-              {fullName && (
-                <p>
-                  Hi! {fullName} <br /> <br /> You have successfully logged{" "}
-                  {loginType}. <br /> <br />
-                  {happyWorking} <br />
-                  <br /> {formattedDate} {""} {formattedTime}
-                </p>
-              )}
-              {qrError && <h1>{qrError}</h1>}
-              {isDuplicate && (
-                <h1>Duplicate Entry. Please scan after 2 minutes</h1>
-              )}
-              <br />
-            </h2>
-          </div>
-        )}
-      </>
+          </h2>
+        </div>
+      )}
     </div>
   );
 }
